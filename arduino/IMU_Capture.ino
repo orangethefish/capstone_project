badb7e4c2c0bc6794a09e0b8d65234dec566e3f1
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2023-12-01 23:58:24 +0700

    modify to put all readings in 1 characteristics

--- arduino/IMU_Capture.ino
+++ arduino/IMU_Capture.ino
@@ -6,28 +6,24 @@
 // BLE UUIDs
 
 #define BLE_UUID_IMU_SERVICE              "1101"
-#define BLE_UUID_ACC_CHAR                 "2101"
-#define BLE_UUID_GYRO_CHAR                "2102"
+#define BLE_UUID_IMU_CHAR                 "2101"
 #define BLE_DEVICE_NAME                           "IMU_Sensor"
 #define BLE_LOCAL_NAME                            "IMU_Sensor"
 // Define Sensors
 double acc_rdg[3],gyr_rdg[3]; //0 is x, 1 is y, 2 is z
-#define ACC_SENSOR_UPDATE_INTERVAL                (20)
-#define GYRO_SENSOR_UPDATE_INTERVAL               (20)
+#define IMU_SENSOR_UPDATE_INTERVAL               (20)
 
 union sensor_data{
   struct __attribute__((packed)) {
-    float values[3]; // float array for data (it holds 3)
+    float values[6]; // float array for data (it holds 3)
     bool updated = false;
   };
-  uint8_t bytes[3 * sizeof(float)]; // size as byte array 
+  uint8_t bytes[6 * sizeof(float)]; // size as byte array 
 };
 
-union sensor_data accData;
-union sensor_data gyroData;
+union sensor_data IMUData;
 BLEService IMUService(BLE_UUID_IMU_SERVICE);
-BLECharacteristic accCharacteristic(BLE_UUID_ACC_CHAR, BLERead | BLENotify, sizeof accData.bytes);
-BLECharacteristic gyroCharacteristic(BLE_UUID_GYRO_CHAR, BLERead | BLENotify, sizeof gyroData.bytes);
+BLECharacteristic IMUCharacteristic(BLE_UUID_IMU_CHAR, BLERead | BLENotify, sizeof IMUData.bytes);
 
 //Create a instance of class LSM6DS3
 LSM6DS3 myIMU(I2C_MODE, 0x6A);  //I2C device address 0x6A
@@ -62,9 +58,8 @@ void setup() {
   } else {
     Serial.println("BLE initialized. Waiting for clients to connect.");
   }
-  for (int i = 0; i < 3; i++) {
-    accData.values[i] = 0;
-    gyroData.values[i] = 0;
+  for (int i = 0; i < 6; i++) {
+    IMUData.values[i] = 0;
   }
 }
 
@@ -72,12 +67,7 @@ void setup() {
 
 void loop() {
   bleTask();
-  if (accSensorTask()) {
-    // accPrintTask();
-  }
-  if (gyroSensorTask()){
-    // gyroPrintTask();
-  }
+  IMUTask();
   if (millis() > last_interval_ms + INTERVAL_MS) {
     last_interval_ms = millis();
     acc_rdg[1]= alpha * acc_rdg[1]  + (1 - alpha) * (myIMU.readFloatAccelX() * CONVERT_G_TO_MS2 + offset[1]);
@@ -101,34 +91,20 @@ void loop() {
 }
 
 /*-----------------------------------------------------------------------------------*/
-
-bool accSensorTask() {
+void IMUTask(){
   static long previousMillis2 = 0;
   unsigned long currentMillis2 = millis();
-  if (currentMillis2 - previousMillis2 < ACC_SENSOR_UPDATE_INTERVAL) {
-    return false;
+  if (currentMillis2 - previousMillis2 < IMU_SENSOR_UPDATE_INTERVAL) {
+    return ;
   }
   previousMillis2 = currentMillis2;
-  accData.values[0] = acc_rdg[0]; // 0.11
-  accData.values[1] = acc_rdg[1]; // 1.13
-  accData.values[2] = acc_rdg[2]; // -1.13
-  accData.updated = true;
-  return accData.updated;
-}
-
-bool gyroSensorTask() {
-  static long previousMillis2 = 0;
-  unsigned long currentMillis2 = millis();
-  static float x = 0.00, y = 0.00, z = 0.00;
-  if (currentMillis2 - previousMillis2 < GYRO_SENSOR_UPDATE_INTERVAL) {
-    return false;
-  }
-  previousMillis2 = currentMillis2;
-  gyroData.values[0] = gyr_rdg[0]; // 0.11
-  gyroData.values[1] = gyr_rdg[1]; // 1.13
-  gyroData.values[2] = gyr_rdg[2]; // -1.13
-  gyroData.updated = true;
-  return gyroData.updated;
+  IMUData.values[0] = acc_rdg[0]; // 0.11
+  IMUData.values[1] = acc_rdg[1]; // 1.13
+  IMUData.values[2] = acc_rdg[2]; // -1.13
+  IMUData.values[3] = gyr_rdg[0]; // 0.11
+  IMUData.values[4] = gyr_rdg[1]; // 1.13
+  IMUData.values[5] = gyr_rdg[2]; // -1.13
+  IMUData.updated = true;
 }
 
 /*-----------------------------------------------------------------------------------*/
@@ -143,15 +119,13 @@ bool setupBleMode() {
   BLE.setAdvertisedService(IMUService);
 
   // BLE add characteristics
-  IMUService.addCharacteristic(accCharacteristic);
-  IMUService.addCharacteristic(gyroCharacteristic);
+  IMUService.addCharacteristic(IMUCharacteristic);
 
   // add service
   BLE.addService(IMUService);
 
   // set the initial value for the characteristic:
-  accCharacteristic.writeValue(accData.bytes, sizeof accData.bytes);
-  gyroCharacteristic.writeValue(gyroData.bytes, sizeof gyroData.bytes);
+  IMUCharacteristic.writeValue(IMUData.bytes, sizeof IMUData.bytes);
 
   // set BLE event handlers
   BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
@@ -172,60 +146,16 @@ void bleTask()
     previousMillis = currentMillis;
     BLE.poll();
   }
-  if (accData.updated) {
-    // Bluetooth does not define accelerometer
-    // Units in G
-    int16_t accelerometer_X = round(accData.values[0] * 100.0);
-    int16_t accelerometer_Y = round(accData.values[1] * 100.0);
-    int16_t accelerometer_Z = round(accData.values[2] * 100.0);
-    accCharacteristic.writeValue(accData.bytes, sizeof accData.bytes);
-    accData.updated = false;
+  if(IMUData.updated){
+    int16_t accelerometer_X = round(IMUData.values[0] * 100.0);
+    int16_t accelerometer_Y = round(IMUData.values[1] * 100.0);
+    int16_t accelerometer_Z = round(IMUData.values[2] * 100.0);
+    int16_t gyro_X = round(IMUData.values[3] * 100.0);
+    int16_t gyro_Y = round(IMUData.values[4] * 100.0);
+    int16_t gyro_Z = round(IMUData.values[5] * 100.0);
+    IMUCharacteristic.writeValue(IMUData.bytes, sizeof IMUData.bytes);
+    IMUData.updated = false;
   }
-
-  if (gyroData.updated) {
-    // Bluetooth does not define gyroscope
-    // Units in dps
-    int16_t gyro_X = round(gyroData.values[0] * 100.0);
-    int16_t gyro_Y = round(gyroData.values[1] * 100.0);
-    int16_t gyro_Z = round(gyroData.values[2] * 100.0);
-    gyroCharacteristic.writeValue(gyroData.bytes, sizeof gyroData.bytes);
-    gyroData.updated = false;
-  }
-}
-/*-----------------------------------------------------------------------------------*/
-
-void accPrintTask() {
-  Serial.print("AccX = ");
-  Serial.print(accData.values[0]);
-  Serial.println(" G");
-
-  Serial.print("AccY = ");
-  Serial.print(accData.values[1]);
-  Serial.println(" G");
-
-  Serial.print("AccZ = ");
-  Serial.print(accData.values[2]);
-  Serial.println(" G");
-
-  Serial.print("Acc. Subscription Status: ");
-  Serial.println(accCharacteristic.subscribed());
-}
-
-void gyroPrintTask() {
-  Serial.print("gyroX = ");
-  Serial.print(gyroData.values[0]);
-  Serial.println(" dps");
-
-  Serial.print("gyroY = ");
-  Serial.print(gyroData.values[1]);
-  Serial.println(" dps");
-
-  Serial.print("gyroZ = ");
-  Serial.print(gyroData.values[2]);
-  Serial.println(" dps");
-
-  Serial.print("Gyro. Subscription Status: ");
-  Serial.println(gyroCharacteristic.subscribed());
 }
 /*-----------------------------------------------------------------------------------*/
 
